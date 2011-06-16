64faad561ac59d5244d2e102e4c42be496267120
Hao SUN <xsunsmile@gmail.com>
2011-06-16 20:11:07 +0900

    typo

--- lib/facter/vpn_ipaddress.rb
+++ lib/facter/vpn_ipaddress.rb
@@ -1,7 +1,7 @@
 
 Facter.add("vpn_ipaddress") do
 	setcode do
-		ipaddress = %x{/usr/bin/find /tmp/ -name host_settings.sh -exec grep vpn {} \\; | head -n1 | awk -F\\" '{print $2}'}
+		ipaddress = %x{/usr/bin/find /tmp/ -type f -name \"*sh\" -exec grep vpn {} \\; | head -n1 | awk -F\\" '{print $2}'}
 		ipaddress.chomp.gsub(/\s/,'') if ipaddress
 	end
 end
